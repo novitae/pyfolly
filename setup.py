f13ef4ef808a66ba244649733d3b5984536676b1
ae <85891169+novitae@users.noreply.github.com>
2025-02-20 20:15:37 +0100

    build is working

--- setup.py
+++ setup.py
@@ -1,34 +1,60 @@
 import sys
 import platform
 from pathlib import Path
+import shutil
 from git import Repo
 from Cython.Build import cythonize
 from setuptools import Extension, setup
 
 current_directory = Path().absolute()
+
 folly_source_path = current_directory / "folly-source"
 folly_python_path = current_directory / "folly"
-assert Repo(current_directory).submodule(folly_source_path.name).module_exists(), \
-    "The `folly-source` submodule is not properly initalized."
+
+with Repo(current_directory) as current_repository:
+    assert current_repository.submodule(folly_source_path.name).module_exists(), \
+        "The `folly-source` submodule is not properly initalized."
+
 folly_py_src_path = folly_source_path / "folly" / "python"
 assert folly_py_src_path.exists(), "Couldn't find the `folly/python` directory in the folly-source submodule."
+with Repo(folly_source_path) as folly_source_repo:
+    latest_release = max(folly_source_repo.tags, key=lambda t: t.commit.committed_datetime)
 
-def copy_file_to(source: Path, destination: Path):
-    assert source.exists()
-    if destination.parent.exists() is False:
-        destination.parent.mkdir(parents=True)
-    with open(source, "rb") as read, open(destination, "wb") as write:
-        write.write(read.read())
+    for path in folly_python_path.iterdir():
+        if path.name != "README.md":
+            if path.is_dir():
+                shutil.rmtree(path)
+            else:
+                path.unlink()
 
-for source, destination in [
-    ((folly_py_src_path / "__init__.pxd"), (folly_python_path / "__init__.pxd")),
+    def copy_file_to(source: Path, destination: Path):
+        assert source.exists()
+        if destination.parent.exists() is False:
+            destination.parent.mkdir(parents=True)
+        content = latest_release.commit.tree[str(source.relative_to(folly_source_path))].data_stream.read()
+        with open(destination, "wb") as write:
+            write.write(content)
 
-    ((folly_py_src_path / "executor.pxd"), (folly_python_path / "executor.pxd")),
-    ((folly_py_src_path / "executor.pyx"), (folly_python_path / "executor.pyx")),
-    ((folly_py_src_path / "ProactorExecutor.h"), (folly_python_path / "python" / "ProactorExecutor.h")),
-    ((folly_py_src_path / "ProactorExecutor.cpp"), (folly_python_path / "python" / "ProactorExecutor.cpp")),
-]:
-    copy_file_to(source=source, destination=destination)
+    for source, destination in [
+        ((folly_py_src_path / "__init__.pxd"), (folly_python_path / "__init__.pxd")),
+
+        ((folly_py_src_path / "executor.pxd"), (folly_python_path / "executor.pxd")),
+        ((folly_py_src_path / "executor.pyx"), (folly_python_path / "executor.pyx")),
+        ((folly_py_src_path / "ProactorExecutor.h"), (folly_python_path / "python" / "ProactorExecutor.h")),
+        ((folly_py_src_path / "ProactorExecutor.cpp"), (folly_python_path / "python" / "ProactorExecutor.cpp")),
+
+        ((folly_py_src_path / "iobuf.pxd"), (folly_python_path / "iobuf.pxd")),
+        ((folly_py_src_path / "iobuf.pyx"), (folly_python_path / "iobuf.pyx")),
+        ((folly_py_src_path / "iobuf.pyi"), (folly_python_path / "iobuf.pyi")),
+        ((folly_py_src_path / "iobuf.h"), (folly_python_path / "python" / "iobuf.h")),
+        ((folly_py_src_path / "iobuf.cpp"), (folly_python_path / "python" / "iobuf.cpp")),
+        ((folly_py_src_path / "iobuf_ext.h"), (folly_python_path / "python" / "iobuf_ext.h")),
+        ((folly_py_src_path / "iobuf_ext.cpp"), (folly_python_path / "python" / "iobuf_ext.cpp")),
+    ]:
+        copy_file_to(source=source, destination=destination)
+    
+    (folly_python_path / "iobuf_ext.h").symlink_to(folly_python_path / "python" / "iobuf_ext.h")
+    (folly_python_path / "iobuf_ext.cpp").symlink_to(folly_python_path / "python" / "iobuf_ext.cpp")
 
 if sys.platform == 'darwin':  # macOS
     if platform.machine() == 'arm64':  # Apple Silicon
@@ -47,7 +73,7 @@ else:  # Other platforms
     raise ValueError(f'Unknown {sys.platform=}')
 
 include_dirs.extend(["."])
-compile_args = ['-std=gnu++20']
+compile_args = ['-std=gnu++20', *([] if sys.version_info < (3, 13) else ['-D_Py_IsFinalizing=Py_IsFinalizing'])]
 base_libraries = ['folly', 'glog', 'double-conversion', 'fmt']
 
 exts = [
@@ -55,10 +81,15 @@ exts = [
         "folly.executor",
         sources=["folly/executor.pyx", "folly/python/ProactorExecutor.cpp"],
         libraries=["folly", "glog"],
-        extra_compile_args=[
-            *compile_args,
-            *([] if sys.version_info < (3, 13) else ['-D_Py_IsFinalizing=Py_IsFinalizing'])
-        ],
+        extra_compile_args=compile_args,
+        include_dirs=include_dirs,
+        library_dirs=library_dirs,
+    ),
+    Extension(
+        "folly.iobuf",
+        sources=["folly/iobuf.pyx", "folly/iobuf_ext.cpp"],
+        libraries=["folly", "glog"],
+        extra_compile_args=compile_args,
         include_dirs=include_dirs,
         library_dirs=library_dirs,
     )
@@ -66,7 +97,7 @@ exts = [
 
 setup(
     name="folly",
-    version=max(Repo(folly_source_path).tags, key=lambda t: t.commit.committed_datetime).name,
+    version=latest_release.name,
     packages=["folly"],
     package_data={"": ["*.pxd", "*.h"]},
     setup_requires=["cython"],
