ed05112ec1709d1a62db4ba507f70aef44328611
ae <85891169+novitae@users.noreply.github.com>
2025-02-21 12:17:25 +0100

    ok

---
+++ prepare_folly.py
@@ -0,0 +1,82 @@
+from pathlib import Path
+from zipfile import ZipFile
+import requests
+import sys
+import platform
+from distutils import log
+
+current_directory = Path().absolute()
+
+for arg in sys.argv:
+    if arg.startswith("--folly-version="):
+        custom_version = arg.split("=", 1).pop()
+        break
+else:
+    custom_version = None
+
+folly_source_filename = "folly-source-{version}.zip"
+folly_python_path = current_directory / "folly"
+folly_py_src_path = Path("./folly/python")
+
+for folly_source_path in Path().glob("folly-source-*.zip"):
+    version = folly_source_path.stem.removeprefix("folly-source-")
+    if custom_version is None or version == custom_version:
+        break
+else:
+    response = requests.get(
+        "https://api.github.com/repos/facebook/folly/releases/{}".format(custom_version or "latest"),
+        allow_redirects=False,
+    )
+    if response.status_code == 404:
+        raise ValueError("Couldn't find any folly release named {}".format(response.url.rsplit("/", 1).pop()))
+    assert response.status_code == 200, f"Unknown status reponse {response.status_code}"
+    content = response.json()
+    version = content["name"]
+    for asset in content.get("assets") or []:
+        if asset["content_type"] == "application/zip":
+            break
+    else:
+        raise ValueError(f'Could not find any zip asset in version {version} of folly.')
+    folly_source_path = current_directory / folly_source_filename.format(version=version)
+    if folly_source_path.exists() is False:
+        sys.stderr.write(f"# Downloading folly release {version} ...")
+        sys.stderr.flush()
+        zip_response = requests.get(asset["browser_download_url"], stream=True)
+        with open(folly_source_path, "wb") as write:
+            for item in zip_response.iter_content():
+                write.write(item)
+        sys.stderr.write(" Done\n")
+        sys.stderr.flush()
+
+if sys.platform == 'darwin':  # macOS
+    if platform.machine() == 'arm64':  # Apple Silicon
+        library_dirs = ['/opt/homebrew/lib']
+        include_dirs = ['/opt/homebrew/include']
+    else:  # Intel macOS
+        library_dirs = ['/usr/lib']
+        include_dirs = ['/usr/include']
+elif sys.platform == 'win32':  # Windows
+    library_dirs = ['C:\\Program Files\\Library\\lib']
+    include_dirs = ['C:\\Program Files\\Library\\include']
+elif sys.platform.startswith('linux'):  # Linux
+    library_dirs = ['/usr/lib', '/usr/local/lib']
+    include_dirs = ['/usr/include', '/usr/local/include']
+else:  # Other platforms
+    raise ValueError(f'Unknown {sys.platform=}')
+
+for include_dir in include_dirs:
+    if (Path(include_dir) / "folly").exists():
+        break
+else:
+    raise FileNotFoundError( 'Could not find the include for folly in any '
+                             'of the include directories:', include_dirs )
+
+
+compile_args = ['-std=gnu++20', *([] if sys.version_info < (3, 13) else ['-D_Py_IsFinalizing=Py_IsFinalizing'])]
+
+
+def copy_file_to(z: ZipFile, source: Path, destination: Path):
+    if destination.parent.exists() is False:
+        destination.parent.mkdir(parents=True)
+    with open(destination, "wb") as write:
+        write.write(z.read(str(source)))
--- setup.py
+++ setup.py
@@ -1,40 +1,21 @@
-import sys
-import platform
-from pathlib import Path
 import shutil
-from git import Repo
+from zipfile import ZipFile
 from Cython.Build import cythonize
 from setuptools import Extension, setup
 
-current_directory = Path().absolute()
+from prepare_folly import (
+    folly_python_path, copy_file_to, include_dirs, folly_source_path,
+    folly_py_src_path, library_dirs, version, compile_args
+)
 
-folly_source_path = current_directory / "folly-source"
-folly_python_path = current_directory / "folly"
+for path in folly_python_path.iterdir():
+    if path.name != "README.md":
+        if path.is_dir():
+            shutil.rmtree(path)
+        else:
+            path.unlink()
 
-with Repo(current_directory) as current_repository:
-    assert current_repository.submodule(folly_source_path.name).module_exists(), \
-        "The `folly-source` submodule is not properly initalized."
-
-folly_py_src_path = folly_source_path / "folly" / "python"
-assert folly_py_src_path.exists(), "Couldn't find the `folly/python` directory in the folly-source submodule."
-with Repo(folly_source_path) as folly_source_repo:
-    latest_release = max(folly_source_repo.tags, key=lambda t: t.commit.committed_datetime)
-
-    for path in folly_python_path.iterdir():
-        if path.name != "README.md":
-            if path.is_dir():
-                shutil.rmtree(path)
-            else:
-                path.unlink()
-
-    def copy_file_to(source: Path, destination: Path):
-        assert source.exists()
-        if destination.parent.exists() is False:
-            destination.parent.mkdir(parents=True)
-        content = latest_release.commit.tree[str(source.relative_to(folly_source_path))].data_stream.read()
-        with open(destination, "wb") as write:
-            write.write(content)
-
+with ZipFile(folly_source_path) as z:
     for source, destination in [
         ((folly_py_src_path / "__init__.pxd"), (folly_python_path / "__init__.pxd")),
 
@@ -51,37 +32,13 @@ with Repo(folly_source_path) as folly_source_repo:
         ((folly_py_src_path / "iobuf_ext.h"), (folly_python_path / "python" / "iobuf_ext.h")),
         ((folly_py_src_path / "iobuf_ext.cpp"), (folly_python_path / "python" / "iobuf_ext.cpp")),
     ]:
-        copy_file_to(source=source, destination=destination)
-    
-    (folly_python_path / "iobuf_ext.h").symlink_to(folly_python_path / "python" / "iobuf_ext.h")
-    (folly_python_path / "iobuf_ext.cpp").symlink_to(folly_python_path / "python" / "iobuf_ext.cpp")
+        copy_file_to(z=z, source=source, destination=destination)
 
-if sys.platform == 'darwin':  # macOS
-    if platform.machine() == 'arm64':  # Apple Silicon
-        library_dirs = ['/opt/homebrew/lib']
-        include_dirs = ['/opt/homebrew/include']
-    else:  # Intel macOS
-        library_dirs = ['/usr/lib']
-        include_dirs = ['/usr/include']
-elif sys.platform == 'win32':  # Windows
-    library_dirs = ['C:\\Program Files\\Library\\lib']
-    include_dirs = ['C:\\Program Files\\Library\\include']
-elif sys.platform.startswith('linux'):  # Linux
-    library_dirs = ['/usr/lib', '/usr/local/lib']
-    include_dirs = ['/usr/include', '/usr/local/include']
-else:  # Other platforms
-    raise ValueError(f'Unknown {sys.platform=}')
+(folly_python_path / "iobuf_ext.h").symlink_to(folly_python_path / "python" / "iobuf_ext.h")
+(folly_python_path / "iobuf_ext.cpp").symlink_to(folly_python_path / "python" / "iobuf_ext.cpp")
 
-for include_dir in include_dirs:
-    if (Path(include_dir) / "folly").exists():
-        break
-else:
-    raise FileNotFoundError( 'Could not find the include for folly in any '
-                             'of the include directories:', include_dirs )
 
-include_dirs.extend(["."])
-compile_args = ['-std=gnu++20', *([] if sys.version_info < (3, 13) else ['-D_Py_IsFinalizing=Py_IsFinalizing'])]
-base_libraries = ['folly', 'glog', 'double-conversion', 'fmt']
+include_dirs = include_dirs.copy() + ["."]
 
 exts = [
     Extension(
@@ -104,7 +61,7 @@ exts = [
 
 setup(
     name="folly",
-    version=latest_release.name,
+    version=version,
     packages=["folly"],
     package_data={"": ["*.pxd", "*.h"]},
     setup_requires=["cython"],
