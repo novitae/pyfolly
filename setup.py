9a5f9aabaac61aa02583c4f1ebb8c3e9a12cdd50
ae <85891169+novitae@users.noreply.github.com>
2025-03-07 20:46:42 +0100

    build and import working fine

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from Cython.Build import cythonize
 from setuptools import Extension as SetuptoolsExtension, setup
 from typing import Iterable
 
-# python3.12 ./build/fbcode_builder/getdeps.py build folly --extra-cmake-defines '{"CMAKE_CXX_FLAGS": "-fcoroutines -std=c++20", "CMAKE_CXX_STANDARD": "20", "BUILD_SHARED_LIBS": "ON"}' --no-tests
+# python3.12 ./build/fbcode_builder/getdeps.py install-system-deps
+# python3.12 ./build/fbcode_builder/getdeps.py build folly --extra-cmake-defines '{"BUILD_SHARED_LIBS": "ON", "CMAKE_CXX_STANDARD": "20", "CMAKE_CXX_FLAGS": "-fcoroutines -fPIC"}' --extra-b2-args "cxxflags=-fPIC" --extra-b2-args "cflags=-fPIC" --allow-system-packages
 
 import sys
 import os
@@ -10,40 +11,14 @@ import shutil
 import subprocess
 import copy
 import glob
-from pathlib import Path
-
-folly_install_prefix = "/private/var/folders/zr/gd_xmzjn5qj1mwyskcqgtrkw0000gn/T/fbcode_builder_getdeps-ZUsersZnZfollyZbuildZfbcode_builder/installed/"
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-_library_dir = os.path.join(script_dir, "folly", "external_libs")
 
-if sys.platform.startswith("darwin"):
-    relative_indicator = "@rpath"
-elif sys.platform.startswith("linux"):
-    relative_indicator = "$ORIGIN"
-else:
-    relative_indicator = None
-
-def fix_lz4_for_unix(external_libs_dir: str):
-    if relative_indicator is None:
-        return
-    
-    liblz4_path = os.path.realpath(glob.glob(f"{external_libs_dir}/liblz4.1.*").pop())
-    liblz4_name = os.path.basename(liblz4_path)
-    libfolly_path = os.path.realpath(glob.glob(f"{external_libs_dir}/libfolly.*").pop())
-
-    print(f"{liblz4_name=}")
-
-    subprocess.run(
-        [
-            "install_name_tool",
-            "-change",
-            liblz4_name,
-            f"{relative_indicator}/{liblz4_name}",
-            libfolly_path
-        ],
-        check=True,
-    )
+subprocess.run(["git", "submodule", "update", "--init", "--recursive"], cwd=script_dir, check=True)
+fbcode_builder = [sys.executable, os.path.join(script_dir, "folly-source", "build", "fbcode_builder", "getdeps.py")]
+subprocess.run([*fbcode_builder, "install-system-deps", "folly"], cwd=script_dir, check=True)
+subprocess.run([*fbcode_builder, "build", "folly", "--extra-cmake-defines", '{"BUILD_SHARED_LIBS": "ON", "CMAKE_CXX_STANDARD": "20", "CMAKE_CXX_FLAGS": "-fcoroutines -fPIC"}', "--extra-b2-args", "cxxflags=-fPIC", "--extra-b2-args", "cflags=-fPIC", "--allow-system-packages"], cwd=script_dir, check=True)
+folly_install_prefix = subprocess.run([*fbcode_builder, "show-inst-dir", "folly"], cwd=script_dir, text=True, capture_output=True).stdout.strip()
 
 def get_folly_py_source():
     folly_source_dir = os.path.join(script_dir, "folly-source")
@@ -94,49 +69,26 @@ def prepare_folly():
         if not os.path.exists(api_path):
             os.symlink(src=sym_src, dst=api_path)
 
-def include_dirs(folly_install_prefix: str):
-    include = []
-    for subdir in os.listdir(folly_install_prefix):
-        subdir_path = os.path.join(folly_install_prefix, subdir)
-        if not os.path.isdir(subdir_path):
-            continue
-        include_path = os.path.join(subdir_path, "include")
-        if os.path.isdir(include_path):
-            include.append(include_path)
-    return include
-
-def copy_libs(folly_install_prefix: str):
-    folly_install = Path(folly_install_prefix)
-    external_libs = Path(_library_dir)
-    external_libs.mkdir(parents=True, exist_ok=True)
-    for built_dep in folly_install.iterdir():
-        built_dep_lib = built_dep / "lib"
-        if not built_dep_lib.exists():
-            continue
-        for file in built_dep_lib.iterdir():
-            if file.is_dir() or file.suffix == ".conf":
-                continue
-            new_file = external_libs / file.name
-            if file.is_symlink():
-                target = file.readlink()
-                new_target = external_libs / target.name
-                if not new_file.exists():
-                    new_file.symlink_to(new_target)
-            else:
-                with open(new_file, "wb") as write:
-                    with open(file, "rb") as read:
-                        while (content := read.read(0x10000)):
-                            write.write(content)
-
-copy_libs(folly_install_prefix)
 prepare_folly()
-fix_lz4_for_unix(external_libs_dir=_library_dir)
 
-_library_dirs = [_library_dir]
-_include_dirs = [".", *include_dirs(folly_install_prefix=folly_install_prefix)]
-_runtime_library_dirs = []
-if relative_indicator is not None:
-    _runtime_library_dirs.append(f'{relative_indicator}/external_libs')
+_library_dirs = [os.path.join(script_dir, "folly"), "/opt/homebrew/lib"]
+_include_dirs = [script_dir, os.path.join(folly_install_prefix, "include"), "/opt/homebrew/include"]
+
+for file_name in os.listdir(os.path.join(folly_install_prefix, "lib")):
+    file = os.path.join(folly_install_prefix, "lib", file_name)
+    if os.path.isdir(file):
+        continue
+    if not (file_name.startswith("libfolly.") and file_name.count(".") == 1):
+        continue
+    absolute_file = os.path.realpath(file)
+    with open(os.path.join(script_dir, "folly", file_name), "wb") as write:
+        with open(absolute_file, "rb") as read:
+            while (content := read.read(0x10000)):
+                write.write(content)
+    break
+else:
+    assert False
+
 def Extension(
     name: str,
     sources: Iterable[str],
@@ -169,7 +121,7 @@ def Extension(
         undef_macros=undef_macros,
         library_dirs=(library_dirs if library_dirs else []) + _library_dirs,
         libraries=(libraries if libraries else []) + ["folly", "glog"],
-        runtime_library_dirs=(runtime_library_dirs if runtime_library_dirs else []) + _runtime_library_dirs,
+        runtime_library_dirs=(runtime_library_dirs if runtime_library_dirs else []), # + _runtime_library_dirs,
         extra_objects=extra_objects,
         extra_compile_args=(extra_compile_args if extra_compile_args else []) + ["-std=c++20"],
         extra_link_args=extra_link_args,
